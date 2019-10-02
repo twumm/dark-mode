eca191bece23dbe771898966ac4587adab49c0dc
Martin Twum Mensah <martint.mensah@gmail.com>
2019-10-02 15:01:33 +0000

    refactor: Use custom hook to trigger dark mode switch

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,23 @@
 import React, { useState } from 'react';
 
+import { useDarkMode } from '../hooks/useDarkMode';
+
 const Navbar = () => {
   const [darkMode, setDarkMode] = useState(false);
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
   };
+  const [isDark, toggleDark] = useDarkMode();
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
       <div className="dark-mode__toggle">
         <div
-          onClick={toggleMode}
-          className={darkMode ? 'toggle toggled' : 'toggle'}
+          // onClick={toggleMode}
+          // className={darkMode ? 'toggle toggled' : 'toggle'}
+          onClick={toggleDark}
+          className={isDark ? 'toggle toggled' : 'toggle'}
         />
       </div>
     </nav>
